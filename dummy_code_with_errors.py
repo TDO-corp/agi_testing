dffb98be135902e8fe0464d3fa3017f55f2493e8
Dmytro TSE <4179417@gmail.com>
2026-01-21 17:16:42 +0200

    dummy code is here

---
+++ dummy_code_with_errors.py
@@ -0,0 +1,24 @@
+def add_numbers(a, b):
+    return a + b
+
+def subtract_numbers(a, b)
+return a - b
+
+print("This is a test"
+
+result = add_numbers(5, x)
+
+result = add_numbers(5, "string")
+
+def multiply_numbers(a, b):
+    return a / b
+
+return "This will never execute"
+print("Unreachable code")
+
+    print("This line is indented incorrectly")
+
+import non_existent_module
+
+number = 5
+number.append(10)
